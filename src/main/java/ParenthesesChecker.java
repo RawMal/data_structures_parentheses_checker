6c9263a7025175d07d99576c2255ce94b697d517
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-08-26 10:21:57 +0100

    Parentheses Checker lab

--- src/main/java/ParenthesesChecker.java
+++ src/main/java/ParenthesesChecker.java
@@ -1,15 +1,57 @@
+import java.util.Arrays;
 import java.util.EmptyStackException;
+import java.util.List;
 import java.util.Stack;
 
 public class ParenthesesChecker {
-    public static void main(String[] args) {
-        Stack<String> checkParentheses = new Stack<String>();
-        checkParentheses.push("test for (lots of (nested)) brackets");
+    //original method passes all the tests at once, but not the required method
+   // public static void main(String[] args) {
+       // Stack<String> checkParentheses = new Stack<String>();
+      //  checkParentheses.push("(nested {mixture)}");
+   // }
+
+   // public boolean checkParentheses(String testString) {
+       // return true;
+   private Stack<Character> testData;
+
+    public ParenthesesChecker(){
+        testData = new Stack<>();
     }
 
     public boolean checkParentheses(String testString) {
-        return true;
+
+        List<Character> openingCharacters = Arrays.asList('(', '{', '[', '<');
+        List<Character> closingCharacters = Arrays.asList(')', '}', ']', '>');
+
+        char[] brokenString = testString.toCharArray();
+
+        for (Character character : brokenString){
+            if (openingCharacters.contains(character)){
+                testData.push(character);
+            }
+            else if (closingCharacters.contains(character)){
+
+                int indexToCheck = closingCharacters.indexOf(character);
+
+                char correspondingOpeningCharacter = openingCharacters.get(indexToCheck);
+
+                if (testData.isEmpty() || !testData.peek().equals(correspondingOpeningCharacter)) {
+                    return false;
+                }
+                else {
+                    testData.pop();
+                }
+            }
+        }
+
+        return testData.isEmpty();
     }
+
+
+
+
+
+}
 
 
     //Stack checker = new Stack();
@@ -18,5 +60,5 @@ public class ParenthesesChecker {
 
 
 
-}
+//}
 
