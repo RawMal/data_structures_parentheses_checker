a546b7cc11f824f8a9551906cb3773606fb09104
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-08-26 09:50:20 +0100

    Parentheses checker

---
+++ src/main/java/ParenthesesChecker.java
@@ -0,0 +1,22 @@
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+public class ParenthesesChecker {
+    public static void main(String[] args) {
+        Stack<String> checkParentheses = new Stack<String>();
+        checkParentheses.push("test for (lots of (nested)) brackets");
+    }
+
+    public boolean checkParentheses(String testString) {
+        return true;
+    }
+
+
+    //Stack checker = new Stack();
+
+    //Stack<String> checkParentheses = new Stack<String>();
+
+
+
+}
+
